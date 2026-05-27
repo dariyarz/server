91fad64617c3c7a82382f0c926d9e62be3ac8030
salamavov123-a11y <salamavov123@gmail.com>
2026-05-28 00:10:55 +0500

    backend

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const BOT_TOKEN = 'TOKEN';
+const CHAT_ID = 'CHAT_ID';
+
+app.post('/sell', async (req, res) => {
+  try {
+    const {
+      server,
+      nickname,
+      virts,
+      price,
+      telegram,
+      telegramId,
+    } = req.body;
+
+    const text = `
+💸 НОВАЯ ЗАЯВКА НА ПРОДАЖУ ВИРТОВ
+
+🖥 Сервер: ${server}
+🎮 Ник в игре: ${nickname}
+👤 Telegram: @${telegram}
+🆔 Telegram ID: ${telegramId}
+💰 Хочет продать: ${virts}
+₽ Нужно выплатить: ${price}₽
+    `;
+
+    await axios.post(
+      `https://api.telegram.org/bot${8602446244:AAFuwaWIKnEi1B-y0NdaCunzjZ0xcg2W44s}/sendMessage`,
+      {
+        chat_id: CHAT_ID,
+        text,
+      }
+    );
+
+    res.json({ success: true });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ success: false });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
